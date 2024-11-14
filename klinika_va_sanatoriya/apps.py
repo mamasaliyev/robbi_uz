b53e6d4a1801521dea73d5447564157a7d005dd3
Behruz <qosimovsayidazim@gmail.com>
2024-11-14 17:28:06 +0500

    robbi

---
+++ klinika_va_sanatoriya/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KlinikaVaSanatoriyaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'klinika_va_sanatoriya'
---
+++ restoranlar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RestoranlarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'restoranlar'
